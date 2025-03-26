0621dac08b21f216146e8456204fb0c6cd3832e3
cutekang <rkddl94@gmail.com>
2025-03-26 16:42:19 +0900

    로그인 전 테스트

--- src/main/java/com/app/contents/ContentsFrontController.java
+++ src/main/java/com/app/contents/ContentsFrontController.java
@@ -16,33 +16,31 @@ import com.app.contents.controller.ContentsPointShopController;
 public class ContentsFrontController extends HttpServlet{
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	super.doGet(req, resp);
-	req.setCharacterEncoding("UTF-8");
-	resp.setContentType("text/html; charset=utf-8;");
-	
-	String controllerName = "/contents/";
-	
-	String target = req.getRequestURI().replace(req.getContextPath() + controllerName, "").split("\\.")[0];
-	Result result = null;
-	
-	if(target.equals("contents-achievement")) {
-		result = new ContentsAchievementController().execute(req, resp);
-	}else if(target.equals("contents-mytree")) {
-		result = new ContentsMyTreeController().execute(req, resp);
-	}else if(target.equals("contents-point-shop")) {
-		result = new ContentsPointShopController().execute(req, resp);
-	}else {
-//		전부 404
-	}
-	
-	if(result != null) {
-		if(result.isRedirect()) {
-			resp.sendRedirect(result.getPath());
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=utf-8;");
+		
+		String controllerName = "/contents/";
+		
+		String target = req.getRequestURI().replace(req.getContextPath() + controllerName, "").split("\\.")[0];
+		Result result = null;
+		
+		if(target.equals("contents-achievement")) {
+			result = new ContentsAchievementController().execute(req, resp);
+		}else if(target.equals("contents-mytree")) {
+			result = new ContentsMyTreeController().execute(req, resp);
+		}else if(target.equals("contents-point-shop")) {
+			result = new ContentsPointShopController().execute(req, resp);
 		}else {
-			req.getRequestDispatcher(result.getPath()).forward(req, resp);
+	//		전부 404
 		}
-	}
-	
+		
+		if(result != null) {
+			if(result.isRedirect()) {
+				resp.sendRedirect(result.getPath());
+			}else {
+				req.getRequestDispatcher(result.getPath()).forward(req, resp);
+			}
+		}
 	}
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doGet(req, resp);
