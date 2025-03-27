683ade3cdd221f41558fa53f1d537fe80497b726
cutekang <rkddl94@gmail.com>
2025-03-27 14:15:15 +0900

    MEMBER PROFILE 작업 중

--- src/main/java/com/app/dto/AlertViewDTO.java
+++ src/main/java/com/app/dto/AlertViewDTO.java
@@ -2,4 +2,6 @@ package com.app.dto;
 
 public class AlertViewDTO {
 
+//	AlertVO
+	
 }
--- src/main/java/com/app/dto/MemberProfileDTO.java
+++ src/main/java/com/app/dto/MemberProfileDTO.java
@@ -1,5 +1,24 @@
 package com.app.dto;
 
+import java.sql.Timestamp;
+
 public class MemberProfileDTO {
 
+//	MemberVO
+	private Long id;
+	private String memberEmail;
+	private String memberNickName;
+	private String memberComment;
+	private String memberName;
+	private String memberGender;
+	private Timestamp memberBirth;
+	private Timestamp memberCreateDate;
+	
+//	MemberImgVO
+	private Long memberImageId;
+	private String memberImageName;
+	private String memberImagePath;
+	
+	public MemberProfileDTO() {;}
+	
 }
